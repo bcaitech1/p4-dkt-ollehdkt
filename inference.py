11608167d2a0ab1ad9e16ef0a6d6fe04272aa4cb
sangki930 <sangki931@gmail.com>
2021-06-03 11:52:30 +0900

    [sangki930] branch commit

---
+++ inference.py
@@ -0,0 +1,24 @@
+import os
+from args import parse_args
+from dkt.dataloader import Preprocess
+from dkt import trainer
+import torch
+def main(args):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    args.device = device
+
+    
+    preprocess = Preprocess(args)
+    print('데이터 로딩')
+    # preprocess.load_test_data(args.test_file_name)
+    preprocess.load_test_data(args.test_file_name)
+    test_data = preprocess.get_test_data()
+    args.infer = True
+
+    trainer.inference(args, test_data)
+    
+
+if __name__ == "__main__":
+    args = parse_args(mode='train')
+    os.makedirs(args.model_dir, exist_ok=True)
+    main(args)
